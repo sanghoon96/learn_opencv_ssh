572595a3d4d9beefc7b37e04ab6539b65f68c3fa
sanghoon96 <sanghoon96@naver.com>
2020-12-10 11:47:38 +0900

    cropandresizeimage

---
+++ CropAndResizeImage.py
@@ -0,0 +1,22 @@
+import cv2 as cv
+import numpy as np
+
+# img = cv.imread("datas/images/shapes.png")
+img = cv.imread("datas/images/lambo.png")
+print(img.shape)
+# (462, 623, 3)
+
+imgResize = cv.resize(img,(1000,500))
+print(imgResize.shape)
+# (500, 1000, 3)
+cv.imshow("Image Resize",imgResize)
+
+print(type(img))
+#<class 'numpy.ndaraay>
+imgCropped = img[46:219,152:495] # image numpy matrix
+
+cv.imshow("Image",img)
+cv.imshow("Image Cropped",imgCropped)
+
+cv.waitKey(0)
+cv.destroyAllWindows()
