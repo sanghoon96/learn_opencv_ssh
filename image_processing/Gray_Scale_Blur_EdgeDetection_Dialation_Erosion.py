0154d4f7d60486af035c87a9d60a482c91ac7a3a
sanghoon96 <koreanson96@naver.com>
2020-12-11 18:11:36 +0900

    sudoku, blur

---
+++ image_processing/Gray_Scale_Blur_EdgeDetection_Dialation_Erosion.py
@@ -0,0 +1,24 @@
+import cv2 as cv
+
+img = cv.imread("datas/images/lena.png")
+
+imgGray = cv.cvtColor(img,cv.COLOR_BGR2GRAY)
+cv.imshow("Gray Scale Image",imgGray)
+
+imgBlur = cv.GaussianBlur(imgGray,(7,7),0)
+cv.imshow("Blur Image",imgBlur)
+
+imgCanny = cv.Canny(img,150,200)
+cv.imshow("Canny Image",imgCanny)
+
+import numpy as np
+kernel = np.ones((5,5),np.uint8)
+
+imgDialation = cv.dilate(imgCanny,kernel,iterations=1)
+cv.imshow("Dialation Image",imgDialation)
+
+imgEroded = cv.erode(imgDialation,kernel,iterations=1)
+cv.imshow("Eroded Image",imgEroded)
+
+cv.waitKey(0)
+cv.destroyAllWindows()
