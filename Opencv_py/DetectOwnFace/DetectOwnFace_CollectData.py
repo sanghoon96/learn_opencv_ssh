e7f13b2ab6ecd097a352e6956b4b6d5cb01158ff
sanghoon96 <koreanson96@naver.com>
2020-12-15 18:45:50 +0900

    18:45

--- Opencv_py/DetectOwnFace/DetectOwnFace_CollectData.py
+++ Opencv_py/DetectOwnFace/DetectOwnFace_CollectData.py
@@ -1,54 +1,53 @@
-from cv2 import cv2 as cv
-import numpy as np
+import os
+import cv2.data as cvdata
+from cv2 import cv2 
 
-face_classifier = cv.CascadeClassifier("datas/haar_cascade_files/haarcascade_frontalface_default.xml")
-
-
-def face_extractor(img):
-
-    gray = cv.cvtColor(img,cv.COLOR_BGR2GRAY)
-    faces = face_classifier.detectMultiScale(gray,1.3,5)
-
-    if faces is():
+def face_detector(frame):
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+    if faces is not None:
+        for (x,y,w,h) in faces: 
+            return frame[y:y+h, x:x+w, :]
+    else:
         return None
 
-    for(x,y,w,h) in faces:
-        cropped_face = img[y:y+h, x:x+w]
+dir_name = os.getcwd() + '/datas/images/faces/'
+if not os.path.exists(dir_name):
+    os.mkdir(dir_name)
 
-    return cropped_face
+face_cascade = cv2.CascadeClassifier(cvdata.haarcascades + 'haarcascade_frontalface_default.xml')
+capture = cv2.VideoCapture(1)
 
-
-cap = cv.VideoCapture(1)
 count = 0
 
-import os
-directory_name = 'datas/images/faces/'
+try:
+    print(f'start, capture.isOpened(): {capture.isOpened()}')
+    while capture.isOpened():
+        ret, frame = capture.read()
+        put_text = ''
 
-if not os.path.exists(directory_name):
-    os.mkdir(directory_name)
+        cropped_area = face_detector(frame)
 
-while cap.isOpened():
-    ret, frame = cap.read()
-    cropped_area = face_extractor(frame)
-    put_text = ''
-    if cropped_area is not None:
-        count+=1
-        area = cv.resize(cropped_area,(200,200))
-        area = cv.cvtColor(area, cv.COLOR_BGR2GRAY)
+        if cropped_area is not None:
+            count += 1
+            area = cv2.resize(cropped_area, (200,200))
+            area = cv2.cvtColor(area, cv2.COLOR_RGB2GRAY)
 
-        file_name = directory_name + 'user'+str(count)+'.jpg'
-        cv.imwrite(file_name,area)
+            file_name = dir_name + 'user' + str(count) + '.jpg'
+            cv2.imwrite(file_name, area)
+            put_text = f"Face Found!, imwrite() count: {count}"
+            cv2.putText(area, put_text, (50,50), cv2.FONT_HERSHEY_COMPLEX, 1, (0,255,0), 2)
+            cv2.imshow('Face Cropped', area)
+        else:
+            put_text = f"Face not Found, imwrite() count: {count}"
 
-        put_text = "Face Found!, imwrite() count" + str(count)
-    else:
-        put_text = "Face not Found"
-
-    cv.putText(area,put_text,(50,50),cv.FONT_HERSHEY_COMPLEX,1,(0,255,0),2)
-    cv.imshow('Face Cropper',area)
-
-    if cv.waitKey(1)==ord('q') or count==50:     # Try adjust count 50, 100, 200
-        break
-
-cap.release()
-cv.destroyAllWindows()
-print('Colleting Samples Complete!!!')
+        cv2.putText(frame, put_text, (50,50), cv2.FONT_HERSHEY_COMPLEX, 1, (0,255,0), 2)
+        cv2.imshow('frame', frame)
+        if cv2.waitKey(1)==ord('q') or count==100:
+            break
+except:
+    print('Error')
+finally:
+    print('finished')
+    capture.release()
+    cv2.destroyAllWindows()
--- Opencv_py/DetectOwnFace/DetectOwnFace_MLTrain.py
+++ Opencv_py/DetectOwnFace/DetectOwnFace_MLTrain.py
@@ -1,18 +1,66 @@
-from cv2 import cv2 as cv
+import os
+import cv2.data as cvdata
+from cv2 import cv2 
 import numpy as np
-from os import listdir
-from os.path import isfile, join
 
-directory_name = 'datas/images/faces/'
-onlyfiles = [f for f in listdir(directory_name) if isfile(join(directory_name,f))]
+def face_detector(frame):
+    face_cascade = cv2.CascadeClassifier(cvdata.haarcascades + 'haarcascade_frontalface_default.xml')
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+    if faces is not None:
+        for (x,y,w,h) in faces: 
+            return frame[y:y+h, x:x+w, :]
+    else:
+        return None
+        
+def train():
+    dir_name = os.getcwd() + '/datas/images/faces/'
+    if not os.path.exists(dir_name):
+        return 0
 
-Training_Data, Labels = list(), list()
-for i, files in enumerate(onlyfiles):
-    image_path = directory_name + onlyfiles[i]
-    images = cv.imread(image_path, cv.IMREAD_GRAYSCALE)
-    Training_Data.append(np.asarray(images, dtype=np.uint8))
-    Labels.append(i)
+    onlyfiles = [f for f in os.listdir(dir_name) if os.path.isfile(os.path.join(dir_name,f))]
 
-Labels = np.asarray(Labels, dtype=np.int32)
-model = cv.face.LBPHFaceRecognizer_create()
-model.train(np.asarray(Training_Data), np.asarray(Labels)) # f(x)= x, y
+    Training_Data, Labels = list(), list()
+    for i, files in enumerate(onlyfiles):
+        image_path = dir_name + files
+        images = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
+        Training_Data.append(np.asarray(images, dtype=np.uint8))
+        Labels.append(i)
+
+    Labels = np.asarray(Labels, dtype=np.int32)
+    model = cv2.face.LBPHFaceRecognizer_create()
+    model.train(np.asarray(Training_Data), np.asarray(Labels))
+    model.save("tmp.yml")
+
+   
+
+def test():
+    model = cv2.face.LBPHFaceRecognizer_create()
+    model.read("tmp.yml")
+    capture = cv2.VideoCapture(1)
+    while capture.isOpened():
+        ret, frame = capture.read()
+        cascade_face = face_detector(frame)
+        try:
+            face = cv2.cvtColor(cascade_face, cv2.COLOR_BGR2GRAY)
+            result = model.predict(face)
+            display_string = ''
+            if result[1] < 500:
+                confidence = int(100*(1-(result[1]/300)))
+                display_string = f'{confidence}% Confidence, '
+            if confidence > 75:
+                display_string = display_string + "Unlocked"
+            else:
+                display_string = display_string + "Locked"
+            cv2.putText(frame, display_string, (250,450), cv2.FONT_HERSHEY_COMPLEX, 1, (0,255,0), 2)
+        except:
+            print("not Found")
+        cv2.imshow("frame", frame)
+        if cv2.waitKey(1)==ord('q'):
+            break
+    capture.release()
+    cv2.destroyAllWindows()
+
+if __name__ == "__main__":
+    # train()
+    test()
